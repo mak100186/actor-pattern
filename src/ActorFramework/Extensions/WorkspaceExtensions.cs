8669e443663232e9436302dbd6a4ce08696f72fd
Muhammed Ali Khan <mak.official@outlook.com>
2025-08-24 13:31:13 +1000

    updates

---
+++ src/ActorFramework/Extensions/WorkspaceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ActorFramework.Abstractions;
+
+namespace ActorFramework.Runtime.Orchestration;
+
+public static class WorkspaceExtensions
+{
+    public static IDirector? GetFirstAvailableDirector(this IWorkspace workspace) => workspace.Directors
+        .OrderBy(d => d.IsBusy()) // Prioritize not busy
+        .FirstOrDefault(d => !d.IsBusy());
+
+    public static IDirector GetLeastLoadedIdleDirector(this IWorkspace workspace) => workspace.Directors
+        .OrderBy(d => d.TotalQueuedMessageCount) // least messages in mailbox
+        .ThenBy(d => d.LastActive) // then by last active time
+        .First();
+}
--- src/ActorFramework/Runtime/Orchestration/WorkspaceLoadBalancer.cs
+++ src/ActorFramework/Runtime/Orchestration/WorkspaceLoadBalancer.cs
@@ -40,15 +40,3 @@ public class WorkspaceLoadBalancer(ActorRegistrationBuilder actorRegistrationBui
         }
     }
 }
-
-public static class WorkspaceExtensions
-{
-    public static IDirector? GetFirstAvailableDirector(this IWorkspace workspace) => workspace.Directors
-        .OrderBy(d => d.IsBusy()) // Prioritize not busy
-        .FirstOrDefault(d => !d.IsBusy());
-
-    public static IDirector GetLeastLoadedIdleDirector(this IWorkspace workspace) => workspace.Directors
-        .OrderBy(d => d.TotalQueuedMessageCount) // least messages in mailbox
-        .ThenBy(d => d.LastActive) // then by last active time
-        .First();
-}
